62ba818511dcd1179444cef955eb75ca87f51fd8
aud04 <aud.stephan@gmail.com>
2015-04-20 11:27:02 +0200

    Modification du controleur d'ajout d'un membre.

--- controler/ajoutMembre_controler.php
+++ controler/ajoutMembre_controler.php
@@ -8,22 +8,6 @@
 
 session_start();
 include "../model/membre_modele.php";
-$Syntaxe = '#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#';
-if (preg_match($Syntaxe, $email)) {
-    return true;
-} else {
-    return false;
-}
-
-if(!(empty($_POST))){
-    if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['pseudo']) || empty($_POST['mdp']) || empty($_POST['droit']))
-    {
-        echo "Un des champs du formulaire est vide \n";
-    }
-    else{
-        echo "L'inscription a été réalisée avec succès. \n";
-    }
-}
 
 $MEMBRE= new Membre($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["pseudo"], $_POST["mdp"],$_POST["droit"]);
 $MEMBRE->InscriptionUti($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["pseudo"], $_POST["mdp"], $_POST["droit"], $_POST["avatar"]);
