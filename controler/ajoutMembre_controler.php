4033d17dd4e2efc579ecc7145e87e8d604a990b1
aud04 <aud.stephan@gmail.com>
2015-04-20 11:33:51 +0200

    Modification du controleur d'ajout d'un membre bis.

--- controler/ajoutMembre_controler.php
+++ controler/ajoutMembre_controler.php
@@ -8,6 +8,22 @@
 
 session_start();
 include "../model/membre_modele.php";
+$Syntaxe = '#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#';
+if (preg_match($Syntaxe, $email)) {
+    return true;
+} else {
+    return false;
+}
+
+if(!(empty($_POST))){
+    if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['pseudo']) || empty($_POST['mdp']) || empty($_POST['droit']))
+    {
+       header ('Location : inscription.php.err=56 '); // retourne une erreur, si tous les champs ne sont pas rempli
+    }
+    else{
+        header ('Location : membre.php '); // dans le cas ou tous est bien rempli.
+    }
+}
 
 $MEMBRE= new Membre($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["pseudo"], $_POST["mdp"],$_POST["droit"]);
 $MEMBRE->InscriptionUti($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["pseudo"], $_POST["mdp"], $_POST["droit"], $_POST["avatar"]);
