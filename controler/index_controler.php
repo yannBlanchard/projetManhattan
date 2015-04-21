63ae960657ee4c3c4940415440871b0bcab38317
Yann <yann.blanchard@u-paris10.fr>
2015-04-21 21:44:27 +0200

    Correction probleme index_controler

--- controler/index_controler.php
+++ controler/index_controler.php
@@ -7,9 +7,13 @@
  */
 
 if(isset($_GET['page'])){
-    $article = recupererArticle($limite,$limite2);
+    $article = recupererArticle((1*$_GET['page'])-1,20*$_GET['page']);
     foreach($article as $cle => $infosArticle){
-
+        $articles[$cle]['titreArticle'] = htmlspecialchars($billet['titreArticle']);
+        $articles[$cle]['corpsArticle'] = htmlspecialchars($billet['corpsArticle']);
+        $articles[$cle]['date_Aticle'] = htmlspecialchars($billet['date_Aticle']);
+        $articles[$cle]['imageArticle'] = htmlspecialchars($billet['imageArticle']);
+        $articles[$cle]['Mem_pseudo'] = htmlspecialchars($billet['Mem_pseudo']);
     }
 }
 else{
